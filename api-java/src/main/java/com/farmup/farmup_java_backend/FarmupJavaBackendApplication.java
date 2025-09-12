efd14be21b47e98195a92b37f5ce7c4bc8b302b3
Antriksh006 <antrikshg007@gmail.com>
2025-09-12 14:16:47 +0530

    fastapi and react working with npm run dev

---
+++ api-java/src/main/java/com/farmup/farmup_java_backend/FarmupJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.farmup.farmup_java_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmupJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmupJavaBackendApplication.class, args);
+	}
+
+}
---
+++ api-java/src/test/java/com/farmup/farmup_java_backend/FarmupJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmup.farmup_java_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmupJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
