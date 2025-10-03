d8440ce03f831f55d583852ed7ff4999e3735f8e
ajinkyashivpure <ajinkyashivpure@gmail.com>
2025-10-03 16:23:35 +0530

    user auth modification

--- api-java/src/main/java/com/farmup/farmup_java_backend/controller/AuthController.java
+++ api-java/src/main/java/com/farmup/farmup_java_backend/controller/AuthController.java
@@ -8,7 +8,6 @@ import com.farmup.farmup_java_backend.service.OtpService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
